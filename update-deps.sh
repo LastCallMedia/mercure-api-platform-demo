67e76ff18040a5c335736e21080d13d7e2411d55
Silas Albrecht <silas.a.a@gmail.com>
2018-12-18 17:28:25 -0500

    Delete stuff we don't need.

--- update-deps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker-compose pull
-docker-compose build
-docker-compose run php composer update
-docker-compose run admin yarn upgrade
-docker-compose run client yarn upgrade
-echo 'Run `docker-compose up --build --force-recreate` now and check that everything is fine!'
